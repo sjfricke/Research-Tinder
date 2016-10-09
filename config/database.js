6bdee5507335b812440a0bce557b68fd96371a03
Spencer <spencer@branch2.com>
2016-10-08 20:39:38 -0500

    update2

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: process.env.AZURE_DB || 'mongodb://127.0.0.1:27017/ResearchTinder',
+  url: process.env.CUSTOMCONNSTR_AZURE_DB || 'mongodb://127.0.0.1:27017/ResearchTinder',
 };
